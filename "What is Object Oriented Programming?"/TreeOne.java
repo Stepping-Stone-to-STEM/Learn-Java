f9ee620c7e78dd212d9e9819a33041b7588b701a
AddieLeigh <138514782+AddieLeigh@users.noreply.github.com>
2023-07-19 17:28:39 -0600

    Clarified comments on code

--- "What is Object Oriented Programming?"/TreeOne.java
+++ "What is Object Oriented Programming?"/TreeOne.java
@@ -1,14 +1,18 @@
-// Create class "TreeOne"
-public class TreeOne {
+// Create class TreeOne
+class TreeOne {
     
-    // Create a new field, "treeOneColor" and assign it the value "green"
+    // Create a new field, treeOneColor, and assign it the value "green"
     String treeOneColor = "green";
 
-    //Call main method
+    // Call the main method (a method built-in to java that tells the computer what it needs to run)
     public static void main(String[] args) {
+
+        // Create a new object of the class TreeOne, called appleTree
         TreeOne appleTree = new TreeOne();
+
+        // Print the value of treeOneColor, inside appleTree, to the system
         System.out.println(appleTree.treeOneColor);
-    }
 
+    } // Main method end
 
-} // "TreeOne" class end
+} // TreeOne class end
