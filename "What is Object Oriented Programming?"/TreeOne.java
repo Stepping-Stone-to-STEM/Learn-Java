747d311f9bab165b87fbf02a16c156fd8e6d38c7
AddieLeigh <138514782+AddieLeigh@users.noreply.github.com>
2023-07-19 16:35:42 -0600

    Add files via upload

---
+++ "What is Object Oriented Programming?"/TreeOne.java
@@ -0,0 +1,14 @@
+// Create class "TreeOne"
+public class TreeOne {
+    
+    // Create a new field, "treeOneColor" and assign it the value "green"
+    String treeOneColor = "green";
+
+    //Call main method
+    public static void main(String[] args) {
+        TreeOne appleTree = new TreeOne();
+        System.out.println(appleTree.treeOneColor);
+    }
+
+
+} // "TreeOne" class end
